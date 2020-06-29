61ac84dccbd83216a7da6b41c41c4e87e30381e7
shir shukrun <shirshukrun1@gmail.com>
2020-06-29 07:38:46 +0300

    installing packages

---
+++ test.R
@@ -0,0 +1,20 @@
+setwd("C:/R/R_test")
+
+#installing libraries
+
+#install.packages('ggplot2')
+#install.packages('dplyr')
+#install.packages('rpart')
+#install.packages('rpart.plot')
+#install.packages('e1071')
+#install.packages('caTools')
+#install.packages('pROC')
+
+library(ggplot2)
+library(dplyr)
+library(rpart)
+library(rpart.plot)
+library(e1071)
+library(caTools)
+library(pROC)
+
